cfb6c308ce906942bba2c7851366c069c3409c69
DevYuns <ydhun93@gmail.com>
2022-01-09 19:37:15 +0900

    Fix github-pages error

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import {svelte} from '@sveltejs/vite-plugin-svelte';
 const production = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
+  base: './',
   clearScreen: false,
   server: {port: 1337},
   plugins: [
