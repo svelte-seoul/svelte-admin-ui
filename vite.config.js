c16d5403a843a89cdeda315eda6e85517adadb6c
DevYuns <ydhun93@gmail.com>
2022-01-10 13:06:09 +0900

    Implement basic installable pwa

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import {VitePWA} from 'vite-plugin-pwa';
 import {defineConfig} from 'vite';
 import preprocess from 'svelte-preprocess';
 import routify from '@roxi/routify/vite-plugin';
@@ -6,11 +7,39 @@ import {svelte} from '@sveltejs/vite-plugin-svelte';
 const production = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  base: './',
+  base: '/',
   clearScreen: false,
   server: {port: 1337},
   plugins: [
-    routify(),
+    VitePWA({
+      useCredentials: true,
+      includeAssets: ['favicon.ico', 'favicon.svg','robots.txt','apple-touch-icon.png'],
+      manifest: {
+        name: 'svelte-admin-ui',
+        short_name: 'admin-ui',
+        description: 'svelte admin ui powered by dooboolab',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),  
+    routify({}),
     svelte({
       emitCss: true,
       compilerOptions: {dev: !production},
