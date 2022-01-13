5e5ed0c7596c122b5c6aee2c504c6216b90fb85b
DevYuns <ydhun93@gmail.com>
2022-01-13 19:03:21 +0900

    Enhance jest setup

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,14 +1,16 @@
 module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/test/jestSetup.ts'],
+  preset: 'ts-jest/presets/js-with-ts',
+  setupFilesAfterEnv: ['<rootDir>/test/jestSetup.ts', '@testing-library/jest-dom/extend-expect'],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|ts)$': 'ts-jest',
     '^.+\\.svelte$': ['svelte-jester', {'preprocess': true}],
   },
-  moduleNameMapper: {'^.+\\.(css|less|scss)$': 'babel-jest'},
+  moduleNameMapper: {'^.+\\.(css|less|scss|svg)$': 'babel-jest', '^(\\.{1,2}/.*)\\.js$': '$1',},
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js?|ts?|svelte?)$',
   moduleFileExtensions: ['js', 'ts', 'svelte'],
   modulePathIgnorePatterns: ['dist', 'build'],
   testEnvironment: 'jsdom',
+  testURL: "http://test.com/auth/sign-in",
   globals: {
     window: {},
     'ts-jest': {
